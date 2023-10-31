952f5d6ed53d8086016dd37f17617fc38208bcaf
hkshakib <humayunkibriashakib@gmail.com>
2023-10-31 21:41:01 +0600

    feat: implemented sidebar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import SideBar from "./Components/SideBar";
 
 function App() {
   return (
-    <div className="flex h-[100%] w-[100%] p-4">
-      <div className="flex basis-[20%] justify-center border border-red-500">
+    <div className="flex h-[100%] w-[100%] p-4 bg-[#f9fafb]">
+      <div className="flex basis-[20%] justify-center">
         <SideBar />
       </div>
-      <div className="flex flex-1 border border-blue-600">Main Contetn</div>
+      <div className="flex flex-1">Main Contetn</div>
     </div>
   );
 }
