466e2a4da56fd99271c343beb69c9785fc69acd6
hkshakib <humayunkibriashakib@gmail.com>
2023-10-31 18:27:28 +0600

    feat: Create sidebar layout

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import SideBar from "./Components/SideBar";
 
 function App() {
   return (
-    <div className="flex h-[100%] w-[100%]">
-      <div className="flex basis-[20%] border border-red-500">
+    <div className="flex h-[100%] w-[100%] p-4">
+      <div className="flex basis-[20%] justify-center border border-red-500">
         <SideBar />
       </div>
       <div className="flex flex-1 border border-blue-600">Main Contetn</div>
--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,15 +1,22 @@
 const SideBar = () => {
   return (
-    <div className="flex flex-col">
-      <div className="text-[30px] text-[#0048ad] font-bold">abc firm</div>
-      <div className="flex flex-col">
-        <button>Home</button>
-        <button>All Files</button>
-        <button>Saved</button>
-        <button>Integrations</button>
-        <button>Trash</button>
-        <button>Settings</button>
-        <button>Help And Support</button>
+    <div className="flex flex-col basis-[100%] justify-center items-center">
+      <div className="flex basis-[8%] text-[30px] text-[#0048ad] font-bold mb-4 mt-4">
+        abc firm
+      </div>
+      <div className="flex h-[600px] flex-col border">
+        <button className="SideBarButton">Home</button>
+        <button className="SideBarButton">All Files</button>
+        <button className="SideBarButton">Saved</button>
+        <button className="SideBarButton">Integrations</button>
+        <button className="SideBarButton">Trash</button>
+        <button className="SideBarButton">Settings</button>
+        <button className="SideBarButton">Help And Support</button>
+      </div>
+      <div className="flex flex-1">
+        <div className="h-[200px] w-[300px] bg-blue-500 mb-4 mt-4 rounded-md">
+          Hello
+        </div>
       </div>
     </div>
   );
